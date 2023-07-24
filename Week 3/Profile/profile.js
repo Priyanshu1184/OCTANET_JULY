4e22219ab02553ca4fe12cbe93ac410f98961784
Priyanshu Sahoo <94666105+Priyanshu1184@users.noreply.github.com>
2023-07-24 19:19:30 +0530

    Add files via upload

---
+++ Week 3/Profile/profile.js
@@ -0,0 +1,10 @@
+
+document.getElementById('profile').addEventListener('click', function(){
+  window.location.href = "signup.html"
+})
+document.getElementById('landingPage').addEventListener('click', function(){
+  window.location.href = "../Homepages/main.html"
+})
+document.getElementById('signUp').addEventListener('click', function(){
+  window.location.href = "../Profile/signup.html"
+})
---
+++ Week 3/Profile/signup.js
@@ -0,0 +1,11 @@
+
+document.getElementById('profile').addEventListener('click', function(){
+    window.location.href = "profile.html"
+  })
+document.getElementById('landingPage').addEventListener('click', function(){
+    window.location.href = "../Homepages/main.html"
+})
+
+document.getElementById('logIn').addEventListener('click', function(){
+    window.location.href = "../Profile/profile.html"
+})
